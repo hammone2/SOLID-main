0420ea086103542252bdf868174ef0b498817dea
hammone2 <hammone2@my.erau.edu>
2025-11-13 20:57:17 -0700

    Initial commit

---
+++ SOLID Project/Assets/_Project/Scripts/SelectionManager.cs
@@ -0,0 +1,50 @@
+﻿﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectionManager : MonoBehaviour
+{
+    [SerializeField] private string selectableTag = "Selectable";
+    [SerializeField] private Material highlightMaterial;
+    [SerializeField] private Material defaultMaterial;
+
+    private Transform _selection;
+
+    private void Awake()
+    {
+        SceneManager.LoadScene("Environment", LoadSceneMode.Additive);
+        SceneManager.LoadScene("UI", LoadSceneMode.Additive);
+    }
+
+    private void Update()
+    {
+        if (_selection != null)
+        {
+            var selectionRenderer = _selection.GetComponent<Renderer>();
+            if (selectionRenderer != null)
+            {
+                selectionRenderer.material = defaultMaterial;
+            }
+        }
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
+        _selection = null;
+        if (Physics.Raycast(ray, out var hit))
+        {
+            var selection = hit.transform;
+            if (selection.CompareTag(selectableTag))
+            {
+                _selection = selection;
+            }
+        }
+
+        if (_selection != null)
+        {
+            var selectionRenderer = _selection.GetComponent<Renderer>();
+            if (selectionRenderer != null)
+            {
+                selectionRenderer.material = highlightMaterial;
+            }
+        }
+    }
+}
